c08044e41addc84027e0799eb5f0688b135ca66b
Nikita Semaev <semaev2014@yandex.ru>
2022-11-08 02:23:16 +0300

    Dividing the functionality into two parts

--- forwarder.py
+++ forwarder.py
@@ -10,13 +10,12 @@ load_dotenv()
 client = TelegramClient('telethon', int(os.environ["TG_API_ID"]), os.environ["TG_API_HASH"])
 client.start()
 TG_CHANNEL_ID = int(os.environ["TG_CHANNEL_ID"])
+BOT_USERNAME = int(os.environ["BOT_USERNAME"])
 
 
 @client.on(NewMessage(chats=(TG_CHANNEL_ID,)))
 async def channel_handler(event: NewMessage.Event):
-    print(event.message.text)
-    trader = Trader()
-    trader.make_order(symbol='GBPJPY', lot=0.1, sl=170, tp=165, buy=False)
+    await client.send_message(BOT_USERNAME, event.message.text)
 
 
 client.run_until_disconnected()
