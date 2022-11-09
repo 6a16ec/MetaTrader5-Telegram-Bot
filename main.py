c19d2ff6d7f6de7d6ab587529d6d4b0b374cc690
Nikita Semaev <semaev2014@yandex.ru>
2022-11-09 04:16:38 +0300

    Changing the logic of order processing, result tracking

--- main.py
+++ main.py
@@ -17,9 +17,13 @@ async def base_handler(message: types.Message):
     if message.chat.id == USER_ID:
         order = Order(message.text)
         if order.is_valid():
-            await message.reply('Order found')
-            make_order(order)
-            await message.reply('Order is made')
+            await message.reply('Order found', reply=False)
+            result1 = make_order(order, tp_number=1)
+            result2 = make_order(order, tp_number=2)
+            if result1 and result2:
+                await message.reply('Request executed successfully', reply=False)
+            else:
+                await message.reply('Problem when executing a request', reply=False)
 
 
 if __name__ == '__main__':
--- trader.py
+++ trader.py
@@ -5,27 +5,28 @@ import MetaTrader5 as mt5
 from order import Order
 
 
-def make_order(order: Order, lot=0.1):
+def make_order(order: Order, lot=0.1, tp_number=1):
     if not mt5.initialize():
         logging.error('Problem with initialize')
-        return
+        return False
     symbol = order.get_symbol()
     symbol_info = mt5.symbol_info(symbol)
     if symbol_info is None:
         logging.error('Problem with symbol_info')
         mt5.shutdown()
-        return
+        return False
     if not symbol_info.visible:
         if not mt5.symbol_select(symbol, True):
             logging.error('Problem with symbol_info.visible')
             mt5.shutdown()
-            return
+            return False
     if order.get_type() == 'BUY':
         price = mt5.symbol_info_tick(symbol).ask
         order_type = mt5.ORDER_TYPE_BUY
     else:
         price = mt5.symbol_info_tick(symbol).bid
         order_type = mt5.ORDER_TYPE_SELL
+    take_profit = order.get_take_profit1() if tp_number == 1 else order.get_take_profit2()
     deviation = 20
     request = {
         "action": mt5.TRADE_ACTION_DEAL,
@@ -34,7 +35,7 @@ def make_order(order: Order, lot=0.1):
         "type":  order_type,
         "price": price,
         "sl": order.get_stop_loss(),
-        "tp": order.get_take_profit2(),
+        "tp": take_profit,
         "deviation": deviation,
         "magic": 234000,
         "comment": "python script open",
@@ -44,3 +45,4 @@ def make_order(order: Order, lot=0.1):
     mt5.order_send(request)
     logging.info('Sent')
     mt5.shutdown()
+    return True
